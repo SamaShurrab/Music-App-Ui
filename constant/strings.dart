1d49d47c2d1a269b5453449e0c6745c9755fca2b
Sama <smshorap@gmail.com>
2025-08-28 20:11:39 +0300

    The application's texts have been added, which are the names of the songs and the singer of the song.

--- constant/strings.dart
+++ constant/strings.dart
@@ -1,3 +1,8 @@
-class AppStrings{
-  
+class AppStrings {
+  final List<Map<String, String>> musicList = [
+    {"musicName": "Holix", "musicSinger": "Flume"},
+    {"musicName": "Never BE LIKE You", "musicSinger": "Flume × Kia"},
+    {"musicName": "Unavailable", "musicSinger": "Davido"},
+    {"musicName": "Say it", "musicSinger": "Flume"},
+  ];
 }

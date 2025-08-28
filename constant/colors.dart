4ed53ac3b5eb65d251598956d69a716700829669
Sama <smshorap@gmail.com>
2025-08-28 20:05:25 +0300

    Adding application colors

--- constant/colors.dart
+++ constant/colors.dart
@@ -1,3 +1,8 @@
-class AppColors{
-  
+import 'dart:ui';
+
+class AppColors {
+  final Color primaryColor = Color(0xffe9effc);
+  final Color secondaryColor = Color(0xff66717c);
+  final Color specialColor = Color(0xff7ea1fd);
+  final Color hoverColor = Color(0xffccdbf2);
 }

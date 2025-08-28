e351c73c2075406d072fcf691b973c40dc36b17d
Sama <smshorap@gmail.com>
2025-08-28 20:16:30 +0300

    add music class contains atrributes for any music

--- constant/strings.dart
+++
@@ -1,8 +0,0 @@
-class AppStrings {
-  final List<Map<String, String>> musicList = [
-    {"musicName": "Holix", "musicSinger": "Flume"},
-    {"musicName": "Never BE LIKE You", "musicSinger": "Flume × Kia"},
-    {"musicName": "Unavailable", "musicSinger": "Davido"},
-    {"musicName": "Say it", "musicSinger": "Flume"},
-  ];
-}
---
+++ lib/class/music.dart
@@ -0,0 +1,11 @@
+class Music {
+  String musicName;
+  String nusicSinger;
+  String musicImage;
+
+  Music({
+    required this.musicName,
+    required this.nusicSinger,
+    required this.musicImage,
+  });
+}
